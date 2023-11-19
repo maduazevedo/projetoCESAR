090e9204b54f4d1c0d9ee5e501780e11c13ee8dc
Eduarda Azevedo <dudafazv@gmail.com>
2023-11-19 00:36:38 -0300

    a

--- cadastro.py
+++ cadastro.py
@@ -1,18 +1,20 @@
 print("LucidApp\n")
 
+#funções para cadastro da empresa CRUD
 
-def criar_empresa():
+def create_empresa():
     cnpj = input("Digite o CNPJ da empresa: ")
     empresa = input("Digite o nome da empresa: ")
     senha = input("Digite a senha da empresa: ")
     
     with open("empresas.txt", "a") as arquivo:
+        #incrementa
         linha = (f'\nCNPJ: {cnpj} Empresa: {empresa} Senha: {senha}\n')
         arquivo.write(linha)
         print("Empresa cadastrada com sucesso!")
 
 
-def listar_empresas():
+def read_empresas():
     try:
         with open("empresas.txt", "r") as arquivo:
             empresas = arquivo.readlines()
@@ -26,31 +28,34 @@ def listar_empresas():
         print("Arquivo de empresas não encontrado.")
 
 
-def atualizar_empresa():
+def update_empresa():
     cnpj = input("Digite o CNPJ da empresa que deseja atualizar: ")
     try:
         with open("empresas.txt", "r") as arquivo:
             linhas = arquivo.readlines()
-            
+            #substitui
             with open("empresas.txt", "w") as arquivo:
                 empresa_encontrada = False
                 for linha in linhas:
-                    if (f'CNPJ: {cnpj}') in linha:
+                    if cnpj in linhas:
                         empresa_encontrada = True
                         empresa = input("Digite o novo nome: ")
                         senha = input("Digite a nova senha: ")
+                        #atualização:
                         linha = (f'\nCNPJ: {cnpj} Empresa: {empresa} Senha: {senha}\n')
                         arquivo.write(linha)
                         print("Empresa atualizada com sucesso!")
                     else:
                         arquivo.write(linha)
-                        if not empresa_encontrada:
-                            print("Empresa não encontrada.")
+                        #nao deixa as informações das linhas serem apagadas
+        if not empresa_encontrada:
+            print("Empresa não encontrada.")
+            
     except FileNotFoundError:
         print("Arquivo de empresas não encontrado.")
 
 
-def deletar_empresa():
+def delet_empresa():
     cnpj = input("Digite o CNPJ da empresa que deseja deletar: ")
     try:
         with open("empresas.txt", "r") as arquivo:
@@ -58,43 +63,61 @@ def deletar_empresa():
             with open("empresas.txt", "w") as arquivo:
                 empresa_encontrada = False
             for linha in linhas:
-                if (f'CNPJ: {cnpj}') in linha:
+                if cnpj in linha:
                     empresa_encontrada = True
                     print(f"Empresa com CNPJ {cnpj} deletada com sucesso!")
                 else:
                     arquivo.write(linha)
-                    if not empresa_encontrada:
-                        print("Empresa não encontrada.")
+                    #nao deixa o arquivo ser apagado
+        if not empresa_encontrada:
+            print("Empresa não encontrada.")
+            
     except FileNotFoundError:
         print("Arquivo de empresas não encontrado.")
 
 
-# Funções para cadastro de usuários
-def criar_usuario():
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Funções para cadastro de usuários CRUD
+def create_usuario():
     nome = input("Digite o nome do usuário: ")
     email = input("Digite o e-mail do usuário: ")
     senha1 = input("Digite a senha do usuário: ")
     with open("usuarios.txt", "a") as arquivo:
+        #incremento
         linha = (f"Nome: {nome}, E-mail: {email}, Senha: {senha1}\n")
         arquivo.write(linha)
-        print("Usuário cadastrado com sucesso!")
+        print("Seja bem vindo(a)", nome)
 
 
-def listar_usuarios():
+def read_usuarios():
     try:
         with open("usuarios.txt", "r") as arquivo:
             usuarios = arquivo.readlines()
             if usuarios:
                 print("Lista de usuários cadastrados:")
                 for usuario in usuarios:
-                    print(usuario)
+                    print("Usuários LucidApp:", usuario)
             else:
                 print("Não há usuários cadastrados.")
     except FileNotFoundError:
         print("Arquivo de usuários não encontrado.")
 
 
-def atualizar_usuario():
+def update_usuario():
     email = input("Digite o e-mail do usuário que deseja atualizar: ")
     try:
         with open("usuarios.txt", "r") as arquivo:
@@ -102,22 +125,24 @@ def atualizar_usuario():
             with open("usuarios.txt", "w") as arquivo:
                 usuario_encontrado = False
                 for linha in linhas:
-                    if f"E-mail: {email}" in linha:
+                    if email in linha:
                         usuario_encontrado = True
                         nome = input("Digite o novo nome: ")
-                        senha1 = input("Digite a nova senha: ")
-                        linha = (f"Nome: {nome}, E-mail: {email}, Senha: {senha1}\n")
+                        email= input("Insira seu novo email: ")
+                        senha = input("Digite a nova senha: ")
+                        linha = (f"Nome: {nome}, E-mail: {email}, Senha: {senha}\n")
                         arquivo.write(linha)
-                        print("Usuário atualizado com sucesso!")
+                        print("Suas informações foram atualizadas com sucesso!")
                     else:
                         arquivo.write(linha)
-                        if not usuario_encontrado:
-                            print("Usuário não encontrado.")
+            if not usuario_encontrado:
+                print("Usuário não encontrado.")
     except FileNotFoundError:
         print("Arquivo de usuários não encontrado.")
 
 
-def deletar_usuario():
+def delet_usuario():
+    
     email = input("Digite o e-mail do usuário que deseja deletar: ")
     try:
         with open("usuarios.txt", "r") as arquivo:
@@ -125,32 +150,41 @@ def deletar_usuario():
             with open("usuarios.txt", "w") as arquivo:
                 usuario_encontrado = False
                 for linha in linhas:
-                    if f"E-mail: {email}" in linha:
+                    if email in linha:
                         usuario_encontrado = True
                         print(f"Usuário com e-mail {email} deletado com sucesso!")
                     else:
                         arquivo.write(linha)
-                        if not usuario_encontrado:
-                            print("Usuário não encontrado")
+        if not usuario_encontrado:
+            print("Usuário não encontrado")
     except FileNotFoundError:
         print("Arquivo de usuários não encontrado.")
+        
+        
+        
+        
+        
+        
+        
+        
 
 
 # MENU PRINCIPAL QUE INTERAGE COM O CRUD DE EMPRESAS E USER
 while True:
     print("\nEscolha uma opção:")
-    print("1. Cadastro de Empresa")
-    print("2. Configurações de Empresas")
-    print("3. Cadastro de Usuário")
-    print("4. Configurações de Usuários")
-    print("5. Menu")
+    print("1. Cadastro de Usuário")
+    print("2. Configurações do Usuário")
+    print("3. Menu")
     
     opcao = input("Opção: ")
     
     if opcao == "1":
-        criar_empresa()
-    
+        create_usuario
+        
     elif opcao == "2":
+        create_usuario()
+        
+    elif opcao == "3":
         while True:
             print("\nEscolha uma opção para empresas:")
             print("6. Listar Empresas")
@@ -161,18 +195,17 @@ while True:
             opcao_empresas = input("Opção: ")
             
             if opcao_empresas == "6":
-                listar_empresas()
+                read_empresas()
             elif opcao_empresas == "7":
-                atualizar_empresa()
+                update_empresa()
             elif opcao_empresas == "8":
-                deletar_empresa()
+                delet_empresa()
             elif opcao_empresas == "9":
                 print("Voltando ao menu principal...")
                 break
             else:
                 print("Opção inválida. Escolha uma opção de 6 a 9.")
-    elif opcao == "3":
-        criar_usuario()
+                
     elif opcao == "4":
         while True:
             print("\nEscolha uma opção para usuários:")
@@ -184,14 +217,14 @@ while True:
             opcao_usuarios = input("Opção: ")
             
             if opcao_usuarios == "6":
-                listar_usuarios()
+                create_usuario()
             elif opcao_usuarios == "7":
-                atualizar_usuario()
+                update_usuario()
             elif opcao_usuarios == "8":
-                deletar_usuario()
+                delet_usuario()
             elif opcao_usuarios == "9":
                 print("Voltando ao menu principal...")
-                break;
+                break
             else:
                 print("Opção inválida. Escolha uma opção de 6 a 9.")
         
--- teste.py
+++ teste.py
@@ -1,10 +1,144 @@
-with open("email.txt", "r") as arquivo:
-	email = arquivo.read()
-print(email)
+print("LucidApp\n")
 
-with open("email.txt", "w") as arquivo:
-	arquivo.write("pythonimpressionador2@gmail.com")
+#funções para cadastro da empresa CRUD
 
-with open("email.txt", "r") as arquivo:
-    email = arquivo.read()
-    print(email)
+def create_usuario():
+    empresa = input("Digite o nome do seu empreendimento: ")
+    cnpj = input("Digite o CNPJ: ")
+    senha = input("Digite a senha : ")
+    print(f'Olá {empresa}, estamos felizes em te ter conosco!')
+    
+    with open("usuarios.txt", "a") as arquivo:
+        #incrementa
+        linha = (f'\n{empresa} CNPJ: {cnpj} Senha: {senha}\n')
+        arquivo.write(linha)
+        print("Cadastro realizado com sucesso!")
+
+def login():
+    cnpj = input("CNPJ: ")
+    senha = input("Senha: ")
+    encontrado = False
+
+    with open("usuarios.txt", "r") as arquivo:
+        for linha in arquivo:
+            dados = linha.strip().split(':')
+            if len(dados) == 3:  # Verifica se há três campos (empresa, CNPJ, senha)
+                empresa, cnpj_armazenado, senha_armazenada = dados
+                if cnpj == cnpj_armazenado and senha == senha_armazenada:
+                    encontrado = True
+                    print("Login bem-sucedido!")
+                    break
+    if not encontrado:
+        print("CNPJ ou senha incorretos. Caso não tenha cadastro, realize-o.")
+        
+        
+
+def read_usuarios():
+    try:
+        with open("usuarios.txt", "r") as arquivo:
+            usuarios = arquivo.readlines()
+            if usuario:
+                print("Lista de usuários cadastrados:")
+                for usuario in usuarios:
+                    print(usuario, "\n")
+            else:
+                print("Não há usuários cadastrados.")
+    except FileNotFoundError:
+        print("Arquivo de usuários não encontrado.")
+
+
+def update_usuario():
+    cnpj = input("Digite o CNPJ que deseja atualizar: ")
+    try:
+        with open("usuarios.txt", "r") as arquivo:
+            linhas = arquivo.readlines()
+            #substitui
+            with open("usuarios.txt", "w") as arquivo:
+                usuario_encontrado = False
+                for linha in linhas:
+                    if cnpj in linhas:
+                        usuario_encontrado = True
+                        empresa = input("Digite o novo nome: ")
+                        senha = input("Digite a nova senha: ")
+                        #atualização:
+                        linha = (f'\n {empresa} CNPJ: {cnpj} Senha: {senha}\n')
+                        arquivo.write(linha)
+                        print("Informações atualizadas com sucesso!")
+                    else:
+                        arquivo.write(linha)
+                        #nao deixa as informações das linhas serem apagadas
+        if not usuario_encontrado:
+            print("Usuário não encontrado.")
+            
+    except FileNotFoundError:
+        print("Arquivo de usuários não encontrado.")
+
+
+def delet_usuario():
+    cnpj = input("Digite o CNPJ que deseja deletar: ")
+    try:
+        with open("usuarios.txt", "r") as arquivo:
+            linhas = arquivo.readlines()
+            with open("usuarios.txt", "w") as arquivo:
+                usuario_encontrado = False
+            for linha in linhas:
+                if cnpj in linha:
+                    usuario_encontrado = True
+                    print(f"Empresa com CNPJ {cnpj} deletada com sucesso!")
+                else:
+                    arquivo.write(linha)
+                    #nao deixa o arquivo ser apagado
+        if not usuario_encontrado:
+            print("CNPJ não encontrado.")
+            
+    except FileNotFoundError:
+        print("Arquivo de usuários não encontrado.")
+
+
+
+
+
+
+
+
+# MENU PRINCIPAL QUE INTERAGE COM O CRUD DE EMPRESAS E USER
+while True:
+    print("\nEscolha uma opção:")
+    print("1. Cadastro de Usuário")
+    print("2. Login de Usuário")
+    print("3. Configurações do Usuário")
+    print("4. Menu")
+    
+    opcao = input("Opção: ")
+    
+    if opcao == "1":
+        create_usuario()
+        
+    elif opcao == "2":
+        login()
+
+    elif opcao == "3":
+        while True:
+            print("\nEscolha uma opção para usuários:")
+            print("6. Listar Usuários")
+            print("7. Atualizar Usuário")
+            print("8. Deletar Usuário")
+            print("9. Voltar")
+            
+            opcao_usuarios = input("Opção: ")
+            
+            if opcao_usuarios == "6":
+                create_usuario()
+            elif opcao_usuarios == "7":
+                update_usuario()
+            elif opcao_usuarios == "8":
+                delet_usuario()
+            elif opcao_usuarios == "9":
+                print("Voltando ao menu principal...")
+                break
+            else:
+                print("Opção inválida. Escolha uma opção de 6 a 9.")
+        
+    elif opcao == "4":
+        print("menu de carlos")
+
